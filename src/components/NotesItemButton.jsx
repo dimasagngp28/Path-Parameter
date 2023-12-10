e53fd410f51c97fb41cfd113130b1eccab680664
dimasagngp28 <100175606+dimasagngp28@users.noreply.github.com>
2023-12-10 19:12:52 +0700

    Add files via upload

---
+++ src/components/NotesItemButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+ 
+function NotesItemButton({ id, onDelete, }) {
+  return (
+    <div className="notes-item-button__container">
+        <button className="notes-item__button-delete" onClick={() => onDelete(id)}>Hapus</button>
+
+    </div>
+  ) 
+}
+
+NotesItemButton.propTypes = {
+  id: PropTypes.number.isRequired,
+  onDelete: PropTypes.func.isRequired,
+}
+export default NotesItemButton;
